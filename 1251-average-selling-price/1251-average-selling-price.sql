3eee0aea8e8ea6ba950b6adcd3711fc66c18169e
‪Saeed Nabawy‬‏ <121503118+saeed174@users.noreply.github.com>
2026-04-07 23:16:38 +0200

    Time: 299 ms (86.54%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,5 @@
+/* Write your T-SQL query statement below */
+select p.product_id , isnull(cast((sum(p.price * u.units *1.0) / sum(u.units))as decimal(10,2)),0) as average_price
+from Prices p left join UnitsSold u
+on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date 
+group by p.product_id 
